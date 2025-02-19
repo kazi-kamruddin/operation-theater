c55b1cb98846d4bff985793e7be7b41266d6afa3
kazi-kamruddin <kazikamruddinahmed@gmail.com>
2025-02-19 20:20:22 +0600

    base

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Routes, Route, Navigate } from "react-router-dom";
 import './App.css'
+import LandingPage from './pages/landingPage.jsx';
 import GreenBeck from './pages/greenbeck.jsx';
 import Experiment from './pages/experiment.jsx';
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Routes>
+          <Route path="/" element={<LandingPage />} /> 
           <Route path="/greenbeck" element={<GreenBeck />} />
           <Route path="/experiment" element={<Experiment />} />
       </Routes>
